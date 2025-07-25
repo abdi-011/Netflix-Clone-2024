e20491b6c657f6dc3e877270d093b45591ca6062
abdi-011 <abdiialemu2@gmail.com>
2025-07-25 23:33:08 +0300

    banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -3,11 +3,13 @@ import './Home.css'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 import Banner from '../../Components/Banner/Banner'
+// import RowList from '../../Components/Rows/RowList/RowList'
 function Home() {
   return (
     <div className='app'>
         <Header/>
-        {/* <Banner/> */}
+        <Banner/>
+        {/* <RowList/> */}
         <Footer/>
     </div>
   )
